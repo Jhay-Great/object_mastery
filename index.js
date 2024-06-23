0d62408f145a0fb86d254c85cc4257af43111184
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-23 15:30:27 +0000

    added function constructor and prototypal inheritance to functionality

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 
 // creating an object for superhero with props like secretIdentity, powers[], weakness
 const superhero = {
-    secretIdentity: 'Captain Man',
-    powers: ['unlimited fire', 'telepathy', 'indestructible'],
-    weakness: 'water',
+    name: 'Kofi Yeboah',
+    secretIdentity: 'Master chef',
+    powers: ['mixologist', 'telepathy', 'kitchen king', 'indestructible', 'knife skills'],
+    weakness: 'always hungry',
 }
 
 // Methods and Functionality:
@@ -16,7 +17,7 @@ const superhero = {
 // o revealIdentity(): Logs the hero’s secret identity
 
 const usePower = function (usePower) {
-    this.powers.map(power => {
+    this.power.map(power => {
         // console.log(power)
         if (power === usePower) {
             console.log(`this is my special power ${power}`)
@@ -35,10 +36,10 @@ superhero[usePower] = usePower;
 superhero[revealIdentity] = revealIdentity; // the this keyword returns undefined here, even tho I'm calling it with an object it returns undefined, find out why
 superhero[reveal] = reveal;
 
-superhero[usePower]('telepathy');
-superhero[usePower]('indestructible')
-console.log(superhero[revealIdentity]());
-console.log(superhero[reveal]());
+// superhero[usePower]('telepathy');
+// superhero[usePower]('indestructible')
+// console.log(superhero[revealIdentity]());
+// console.log(superhero[reveal]());
 
 // const activatePower = usePower.bind(superhero)
 // activatePower('telepathy');
@@ -48,3 +49,30 @@ console.log(superhero[reveal]());
 // Object Constructors:
 // • Task: Create a Superhero constructor function to streamline the creation of
 // multiple superheroes.
+
+const factoryObjects = function({name, secretIdentity, power, weakness}) {
+    
+}
+
+
+const Superhero = function(name, secretIdentity, power, weakness) {
+    // this.object = object;
+    this.name = name;
+    this.secretIdentity = secretIdentity;
+    this.power = power;
+    this.weakness = weakness;
+
+}
+Superhero.prototype.usePower = usePower;
+Superhero.prototype.revealIdentity = revealIdentity;
+
+
+
+
+const vapourize = new Superhero('Dennis King', 'vapourize', ['cooling', 'freezing', 'mist'], 'fire');
+const dashKid = new Superhero('Evan lewis Jr.', 'vapourize', ['cooling', 'freezing', 'mist'], 'fire');
+const monsterHunter = new Superhero('Vanessa Khan', 'Monster Hunter', ['cooling', 'freezing', 'mist'], 'fire');
+console.log(spies);
+console.log(spies.revealIdentity());
+spies.usePower('cooling');
+
