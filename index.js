0793f26d8db8daf9e11025e594467cd9b7e67292
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-23 17:54:47 +0000

    object iteration and transformation done

--- index.js
+++ index.js
@@ -36,23 +36,9 @@ superhero[usePower] = usePower;
 superhero[revealIdentity] = revealIdentity; // the this keyword returns undefined here, even tho I'm calling it with an object it returns undefined, find out why
 superhero[reveal] = reveal;
 
-// superhero[usePower]('telepathy');
-// superhero[usePower]('indestructible')
-// console.log(superhero[revealIdentity]());
-// console.log(superhero[reveal]());
-
-// const activatePower = usePower.bind(superhero)
-// activatePower('telepathy');
-
-// superhero.usePower()
-
-// Object Constructors:
-// • Task: Create a Superhero constructor function to streamline the creation of
-// multiple superheroes.
-
 
 const createCharacter = function(type) {
-    const Superhero = function(name, secretIdentity, power, weakness) {
+    const SuperHumans = function(name, secretIdentity, power, weakness) {
         this.type = type
         this.name = name;
         this.secretIdentity = secretIdentity;
@@ -60,53 +46,51 @@ const createCharacter = function(type) {
         this.weakness = weakness;
     
     }
-    Superhero.prototype.usePower = usePower;
-    Superhero.prototype.revealIdentity = revealIdentity;
-    Superhero.prototype.speak = function(speech) {
-        console.log(`Your ${this.power} won't work against me`);
+    SuperHumans.prototype.usePower = usePower;
+    SuperHumans.prototype.revealIdentity = revealIdentity;
+    SuperHumans.prototype.speak = function(speech) {
+        console.log(`I ${this.secretIdentity} am the strongest super human alive`);
     }
 
-    return Superhero;    
+    return SuperHumans;    
 
 }
 
 const Hero = createCharacter('hero');
 const Villain = createCharacter('villain');
 
-const superman = new Hero("Superman", "Clark Kent", ["Flight", "Super Strength", "X-Ray Vision"], "Kryptonite")
-const lexLuthor = new Villain("Lex Luthor", "Lex Luthor", ["Genius Level Intellect", "Wealth"], "Kryptonite Envy");
+// heros
+const superman = new Hero( "Clark Kent", "Superman",["Flight", "Super Strength", "X-Ray Vision"], "Kryptonite")
+const captainPlanet = new Hero("Obed Manu", "Captain Planet", ["Flight", "Super Strength", "X-Ray Vision"], "Kryptonite")
+const ScoobyDoo = new Hero("Caleb Boadi", "Scooby Doo", ["Flight", "Super Strength", "X-Ray Vision"], "Kryptonite")
 
-console.log('hero: ', superman, 'villain: ', lexLuthor)
+// villains
+const lexLuthor = new Villain( "Lex Luthor", "Lex Luthor",["Genius Level Intellect", "Wealth"], "Kryptonite Envy");
+const thanos = new Villain("Mike Mensah", "Thanos", ["Genius Level Intellect", "Wealth"], "Kryptonite Envy");
+const cowGirl = new Villain("Felicia Yeboah", "Cow Girl", ["Genius Level Intellect", "Wealth"], "Kryptonite Envy");
 
-const Superhero = function(name, secretIdentity, power, weakness) {
-    this.name = name;
-    this.secretIdentity = secretIdentity;
-    this.power = power;
-    this.weakness = weakness;
-
-}
-Superhero.prototype.usePower = usePower;
-Superhero.prototype.revealIdentity = revealIdentity;
-Superhero.prototype.speak = function(speech) {
-    console.log(`Your ${this.power} won't work against me`);
-}
+// console.log(superman.revealIdentity())
 
 
+const superHumans = [superman, captainPlanet, ScoobyDoo, lexLuthor, thanos, cowGirl];
+const playHero = function(characters) {
+    // forEach
+    characters.forEach(character => {
+        if (character.type !== 'villain') return;
+        character.speak();
+    })
+    // map
+    characters.map(character => {
+        if (character.type !== 'hero') return;
+        
+        character.title = 'Defender of the universe';
+    });
 
 
-const vapourize = new Superhero('Dennis King', 'vapourize', ['cooling', 'freezing', 'mist'], 'fire');
-const dashKid = new Superhero('Evan lewis Jr.', 'vapourize', ['cooling', 'freezing', 'mist'], 'fire');
-const monsterHunter = new Superhero('Vanessa Khan', 'Monster Hunter', ['cooling', 'freezing', 'mist'], 'fire');
-// console.log(vapourize);
-// console.log(vapourize.revealIdentity());
-// vapourize.usePower('cooling');
-
-// Object Iteration and Transformation:
-// • Task: Use forEach, map, or filter to manipulate an array of superheroes and
-// supervillains
-
-const playHero = function(...heros) {
+    // filter
+    const heros = characters.filter(character => character.type === 'hero');
     console.log(heros);
 }
-playHero(vapourize, dashKid, monsterHunter);
+
+playHero(superHumans);
 
